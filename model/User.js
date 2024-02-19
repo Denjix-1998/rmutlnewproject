91ff1329b1ebee534b0ca528daad1dfcc603e4f3
nanrmutl <lostboy783@hotmail.com>
2024-02-19 21:22:45 +0700

    first commit

--- model/User.js
+++ model/User.js
@@ -2,11 +2,18 @@ const mongoose = require("mongoose");
 
 const UserSchema = new mongoose.Schema({
   name: String,
-  price: String,
+  specie: String,
   volume: Number,
+  age: String,
+  history: String,
+  weight: String,
   mon: String,
   dad: String,
-  score: String,
+  date: String,
+  human: String,
+  placeborn: String,
+  placefeed: String,
+  health: String,
 });
 
 const UserModel = mongoose.model("users", UserSchema);
